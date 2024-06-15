0eb9dc35a7fbd833d53ce6aad8e184ce67c6a6f2
Anya <vann2299@yandex.ru>
2024-06-15 17:02:25 +0300

    fixes: main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const { PORT } = process.env;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Kupipodariday Service')
-    .setDescription('Kupipodariday backend')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('/api/docs', app, document);
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(PORT);
 }
 bootstrap();
