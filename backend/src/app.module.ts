efd7cf5087707e08ec22f6f3cd4a153ec627826a
Anya <vann2299@yandex.ru>
2024-06-15 14:42:17 +0300

    configured docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,37 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { WishlistsModule } from './wishlists/wishlists.module';
-import { WishesModule } from './wishes/wishes.module';
-import { OffersModule } from './offers/offers.module';
 import { AuthModule } from './auth/auth.module';
-import { typeOrmConfig } from './config/config';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
+// import config from './config';
+
+const { POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_USER, POSTGRES_HOST } =
+  process.env;
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
-    WishlistsModule,
     WishesModule,
+    WishlistsModule,
     OffersModule,
     AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: POSTGRES_HOST,
+      port: 5432,
+      username: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DB,
+      entities: ['dist/*/entities/*.entity.js'],
+      schema: 'public',
+      synchronize: true,
+    }),
   ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
