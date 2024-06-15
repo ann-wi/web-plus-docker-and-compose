5259fa43b5fc114fb9d970e4800794a44df7313d
Anya <vann2299@yandex.ru>
2024-06-15 15:09:32 +0300

    edited api url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:4004";
+export const URL = "https://api.kpd-docker.nomorepartiesco.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
